371e85f703a3cda7173e72980cdd8516d05c1057
Cristian <cristianmatias52@gmail.com>
2020-07-18 18:22:34 +0200

    Finished project

--- src/Model/Bill/Bill.java
+++ src/Model/Bill/Bill.java
@@ -1,15 +1,12 @@
 package Model.Bill;
 
-import Model.Product.Product;
 import Model.User.User;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 import javax.persistence.Basic;
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
